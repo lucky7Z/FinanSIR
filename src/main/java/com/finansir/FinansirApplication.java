ce64dcba4490cbf91090074557e2ea80d0ae01bb
Administrator <Administrator@DESKTOP-099UQRG>
2020-10-24 14:38:04 -0400

    'working' finansir code

---
+++ src/main/java/com/finansir/FinansirApplication.java
@@ -0,0 +1,13 @@
+package com.finansir;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FinansirApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(FinansirApplication.class, args);
+	}
+
+}
