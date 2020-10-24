8b59a0b288ebdf2c1ddad7a8af1f160ede0daa68
alongemo <71347001+alongemo@users.noreply.github.com>
2020-10-24 15:16:52 -0400

    Add files via upload
    Math class with some declarations preparing for the calculations.

---
+++ Math.java
@@ -0,0 +1,35 @@
+/**
+ * 
+ */
+package com.finansir;
+
+/**
+ * @author Administrator
+ *
+ */
+public class Math {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		//Declarations
+		
+		//Inputs
+		int mortgageAmount;
+		double interestRate;
+		int mPeriod;
+		
+		//Outputs
+		int monthlyPayment;
+		int homeInsurance;
+		int propertyTax;
+		int hoaFee;
+		int Total;
+		
+
+	}
+
+}
